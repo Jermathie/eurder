4f136c7ba78ea069391985676816ab81ddb3bf7a
Jermathie <jermathie@gmail.com>
2020-10-28 14:47:54 +0100

    Story 7 tested

--- src/test/java/com/switchfully/jeremie/eurder/services/users/CustomerServiceTest.java
+++ src/test/java/com/switchfully/jeremie/eurder/services/users/CustomerServiceTest.java
@@ -1,11 +1,17 @@
 package com.switchfully.jeremie.eurder.services.users;
 
+import com.switchfully.jeremie.eurder.domain.users.Admin;
 import com.switchfully.jeremie.eurder.domain.users.Customer;
+import com.switchfully.jeremie.eurder.exceptions.AdminPrivilegeException;
 import com.switchfully.jeremie.eurder.repesitory.UserDatabase;
 import com.switchfully.jeremie.eurder.services.CustomerService;
 import com.switchfully.jeremie.eurder.services.UserValidationService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 class CustomerServiceTest {
     @Test
@@ -120,6 +126,69 @@ class CustomerServiceTest {
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> customerService.registerCustomer(customer));
     }
+
+    @Test
+    void givenListOfCustomers_whenAdminChecks_CustomersAreReturned() {
+        UserDatabase userDatabase = new UserDatabase();
+        UserValidationService userValidationService = new UserValidationService();
+        CustomerService customerService = new CustomerService(userDatabase, userValidationService);
+        Admin admin = new Admin(UUID.fromString("96e35a5e-bdbd-492b-aafb-8b9d2c6e96b9"), "admin", "admin", "admin@admin.com");
+        userDatabase.addAdmin(admin);
+
+        Customer customer1 = new Customer("John", "Doe", "johndoe@gmail.com", "BeCentral", "1",
+                "1000", "Brussels", "023522536");
+        Customer customer2 = new Customer("John", "Smith", "johndoe@gmail.com", "BeCentral", "1",
+                "1000", "Brussels", "023522536");
+        customerService.registerCustomer(customer2);
+        customerService.registerCustomer(customer1);
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(customer1);
+        customerList.add(customer2);
+
+        Assertions.assertTrue(customerService.getAllCustomers("96e35a5e-bdbd-492b-aafb-8b9d2c6e96b9").containsAll(customerList));
+    }
+    @Test
+    void givenListOfCustomers_whenSomeoneOtherThanAdminChecks_ThrowAdminPrivilegeException() {
+        UserDatabase userDatabase = new UserDatabase();
+        UserValidationService userValidationService = new UserValidationService();
+        CustomerService customerService = new CustomerService(userDatabase, userValidationService);
+        Customer notAdmin = new Customer(UUID.fromString("1f8767ee-e926-4b5a-bcdd-cb6aa3c04c21"), "John", "Doe", "johndoe@gmail.com", "BeCentral", "1",
+                "1000", "Brussels", "023522536");
+        userDatabase.addCustomer(notAdmin);
+
+        Customer customer1 = new Customer("John", "Doe", "johndoe@gmail.com", "BeCentral", "1",
+                "1000", "Brussels", "023522536");
+        Customer customer2 = new Customer("John", "Smith", "johndoe@gmail.com", "BeCentral", "1",
+                "1000", "Brussels", "023522536");
+        customerService.registerCustomer(customer2);
+        customerService.registerCustomer(customer1);
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(customer1);
+        customerList.add(customer2);
+
+        Assertions.assertThrows(AdminPrivilegeException.class, () -> customerService.getAllCustomers("1f8767ee-e926-4b5a-bcdd-cb6aa3c04c21"));
+    }
+    @Test
+    void givenListOfCustomers_whenCheckersIdDoesntExist_ThrowIllegalArgumentException() {
+        UserDatabase userDatabase = new UserDatabase();
+        UserValidationService userValidationService = new UserValidationService();
+        CustomerService customerService = new CustomerService(userDatabase, userValidationService);
+
+        Customer customer1 = new Customer("John", "Doe", "johndoe@gmail.com", "BeCentral", "1",
+                "1000", "Brussels", "023522536");
+        Customer customer2 = new Customer("John", "Smith", "johndoe@gmail.com", "BeCentral", "1",
+                "1000", "Brussels", "023522536");
+        customerService.registerCustomer(customer2);
+        customerService.registerCustomer(customer1);
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(customer1);
+        customerList.add(customer2);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> customerService.getAllCustomers("1f8767ee-e926-4b5a-bcdd-cb6aa3c04c21"));
+    }
+
+
+
 
 
 
